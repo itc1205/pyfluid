e7e79a629603540714b0c3616ede9d59220073db
Ivan <ivan120536@gmail.com>
2024-02-27 22:21:07 +0400

    fix(loader): Fixed bug with library not loading

--- src/pyfluid/binds.py
+++ src/pyfluid/binds.py
@@ -1,7 +1,7 @@
 from ctypes.util import find_library
 from ctypes import cdll, c_int, c_char_p
 
-__library = load_fluidsynth_library()
+__library = None
 
 
 def load_fluidsynth_library():
@@ -10,7 +10,11 @@ def load_fluidsynth_library():
         raise Exception("Fluidsyth lib is nowhere to be found!")
     return cdll.LoadLibrary(c_fluidsynth_lib_name)
 
+
 # Fluidsynth log bind for testing purposes
 def fluid_log(message: bytes):
     # Fluid log has this defenition fluid_log(int LOG_LEVEL, char* message)
     __library.fluid_log(c_int(3), c_char_p(message))
+
+## TODO: Fix this hackey way of loading libraries
+__library = load_fluidsynth_library()
